65cd124aca892cc21bebf0ede0d1757817fd2da5
akborana <73084981+akborana@users.noreply.github.com>
2021-05-06 12:54:31 +0530

    Update __init__.py

--- Villain/modules/sql/__init__.py
+++ Villain/modules/sql/__init__.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
-from ViLlain import DB_URI
+from Villain import DB_URI
 
 
 def start() -> scoped_session:
